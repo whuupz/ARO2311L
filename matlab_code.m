86b16c221fdb4ad849fb4a91c98cc3d45a3b9d92
whuupz <119022284+whuupz@users.noreply.github.com>
2023-11-15 21:45:49 -0800

    Update matlab_code.m

--- matlab_code.m
+++ matlab_code.m
@@ -9,7 +9,7 @@ clc;
 
 % Set up the serial port
 
-uno = arduinoUno('/dev/cu.usbmodem1414301',9600);
+uno = arduinoUno('/dev/cu.usbmodem1414201',9600);
 
 try
     % Open the serial port
@@ -38,7 +38,8 @@ try
 
           tempurature = extractDataValues(data);
           disp('Tempurature:');
-          disp(tempurature);
+          disp(((9/5) * (tempurature + 32)));
+
           
         elseif (data(11) == 'L')
 
@@ -56,7 +57,7 @@ try
 
            pressure = extractDataValues(data);
            disp('Pressure:');
-           disp(pressure);
+           disp(pressure * 0.00986923);
           
         elseif (data(11) == 'M')
 
@@ -64,6 +65,15 @@ try
             disp('Magnetic Field:');
             disp(magneticField);
           
+        elseif (data(11) == 'Q')
+
+            [quat.x,quat.y,quat.z,quat.w] = extractDataValues2(data);
+            disp('Quaternion:');
+            disp(quat);
+            euler = quat2eul(quat);
+            disp('Euler');
+            disp(euler);
+
         end
 
 
@@ -106,13 +116,45 @@ function [xVal,yVal,zVal] = extractDataValues(data)
     dataZsplit = strsplit(dataZ, ' ');
 
     dataZsplit2 = strsplit(char(dataZsplit(1)), '.');
-
     dataZsplit3 = [char(string(dataZsplit2(1))),'.',char(string(dataZsplit2(2)))];
 
     dataZsplit4 = strsplit((dataZsplit3), '¿');
 
+    dataZsplit5 = strsplit(char(string(dataZsplit4(1))), ' ');
+    dataZsplit6 = strsplit(char(string(dataZsplit5(1))), '');
+    dataZsplit7 = strsplit(char(string(dataZsplit6(1))), 'z');
+
     xVal = double(string(char(dataXsplit(1))));
     yVal = double(string(char(dataYsplit(1))));
-    zVal = round(double(string(dataZsplit4(1))),2);
+    zVal = round(double(string(dataZsplit7(1))),2);
+
+end
+
+function [xVal,yVal,zVal,wVal] = extractDataValues2(data)
+
+    dataArray = strsplit(data, '=');
+
+    dataX = char(dataArray(2));
+    dataY = char(dataArray(3));
+    dataZ = char(dataArray(4));
+    dataW = char(dataArray(5));
+
+    dataXsplit = strsplit(dataX, ' ');
+    dataYsplit = strsplit(dataY, ' ');
+    dataZsplit = strsplit(dataZ, ' ');
+    dataWsplit = strsplit(dataW, ' ');
+
+    dataWsplit2 = strsplit(char(dataWsplit(1)), '.');
+    dataWsplit3 = [char(string(dataWsplit2(1))),'.',char(string(dataWsplit2(2)))];
+
+    dataWsplit4 = strsplit((dataWsplit3), '¿');
+
+    dataWsplit5 = strsplit(char(string(dataWsplit4(1))), ' ');
+    dataWsplit6 = strsplit(char(string(dataWsplit5(1))), '');
+
+    xVal = double(string(char(dataXsplit(1))));
+    yVal = double(string(char(dataYsplit(1))));
+    zVal = double(string(char(dataZsplit(1))));
+    wVal = round(double(string(dataWsplit6(1))),2);
 
 end
