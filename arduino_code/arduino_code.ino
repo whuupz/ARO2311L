0180ac9141cebdb8122a3f6e01d07d8c1c47cd90
whuupz <119022284+whuupz@users.noreply.github.com>
2023-11-16 02:28:47 -0800

    Update arduino_code.ino

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,11 +1,3 @@
-// Arduino9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Arduino9x_TX
-
 #include <SPI.h>
 #include <RH_RF95.h>
 
@@ -13,13 +5,10 @@
 #define RFM95_RST 2
 #define RFM95_INT 3
 
-// Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
 
-// Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-// Blinky on receipt
 #define LED 13
 
 void setup() {
@@ -37,15 +26,10 @@ void setup() {
     while (1);
   }
 
-  Serial.println("LoRa radio init OK!");
-
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
     while (1);
   }
-
-  Serial.print("Set Freq to: ");
-  Serial.println(RF95_FREQ);
 
   rf95.setTxPower(23, false);
 
@@ -54,7 +38,6 @@ void setup() {
 
 void loop() {
   if (rf95.available()) {
-    // Should be a message for us now
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len)) {
@@ -62,8 +45,6 @@ void loop() {
 
       Serial.print("Received: ");
       Serial.println((char*)buf);
-
-      // Process the received data as needed
 
       digitalWrite(13, LOW);
     } else {
