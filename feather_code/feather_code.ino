1458369c02b085d7db1f4134f83430aac23a6c27
whuupz <119022284+whuupz@users.noreply.github.com>
2023-11-16 02:27:15 -0800

    Update feather_code.ino

--- feather_code/feather_code.ino
+++ feather_code/feather_code.ino
@@ -14,6 +14,7 @@ uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
 //                                   id, address
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
 
+
 #define SENSOR1_ADDRESS 0xC0  //pressure sensor address   / to stop 0x12
 #define SENSOR2_ADDRESS 0x28   // to stop 0x40    imu addresses
 
@@ -59,42 +60,73 @@ void setup() {
 int16_t packetnum = 0;
 
 void loop() {
-  readSensor1Data();
-  readSensor2Data();
-  // Add your main loop logic here
-}
 
-void readSensor2Data() {
-  digitalWrite(13, HIGH);
-  delay(10);
-  
-  float pressureKPA = 0, temperatureC = 0;    
-  pressureKPA = mpl115a2.getPressure();  
-  Serial.print("Pressure (kPa): "); Serial.print(pressureKPA, 4); Serial.println(" kPa");
-  sendRadioTransmission("Pressure:", pressureKPA, pressureKPA, pressureKPA);
+  sensors_event_t accelData;
+  bno.getEvent(&accelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+  double accelX = accelData.acceleration.x;
+  double accelY = accelData.acceleration.y;
+  double accelZ = accelData.acceleration.z;
+  sensors_event_t magData;
+  bno.getEvent(&magData, Adafruit_BNO055::VECTOR_MAGNETOMETER); 
+  double magX = magData.magnetic.x;
+  double magY = magData.magnetic.y;
+  double magZ = magData.magnetic.z;
+  sensors_event_t gyroData;
+  bno.getEvent(&gyroData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  double gyroX = gyroData.gyro.x;
+  double gyroY = gyroData.gyro.y;
+  double gyroZ = gyroData.gyro.z;
 
-  temperatureC = mpl115a2.getTemperature();  
-  Serial.print("Temp (*C): "); Serial.print(temperatureC, 1); Serial.println(" *C");
-  sendRadioTransmission("Temp:", temperatureC, temperatureC, temperatureC);
-  
-  digitalWrite(13, LOW);
-}
-
-void readSensor1Data() {
-  sensors_event_t orientationData, angVelocityData, linearAccelData, magnetometerData, accelerometerData, gravityData;
+  sensors_event_t orientationData;
   bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
-  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
-  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
-  bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
-  bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
+  double orientationX = orientationData.orientation.x;
+  double orientationY = orientationData.orientation.y;
+  double orientationZ = orientationData.orientation.z;
 
-  printEvent(&orientationData);
-  printEvent(&angVelocityData);
-  printEvent(&linearAccelData);
-  printEvent(&magnetometerData);
-  printEvent(&accelerometerData);
-  printEvent(&gravityData);
+  imu::Quaternion quat = bno.getQuat();
+
+  //delay(100);
+  //digitalWrite(13, HIGH);
+  float pressureKPA = 0, temperatureC = 0;
+  pressureKPA = mpl115a2.getPressure();
+  temperatureC = mpl115a2.getTemperature();
+  //digitalWrite(13, LOW);
+  //delay(100);
+
+  char radiopacket[200]; // Adjust the array size accordingly
+  sprintf(radiopacket,
+        "Q:%.2f,%.2f,%.2f,%.2f|A:%.2f,%.2f,%.2f|M:%.2f,%.2f,%.2f|G:%.2f,%.2f,%.2f|O:%.2f,%.2f,%.2f|T:%.2f|P:%.2f|",
+        quat.x(), quat.y(), quat.z(), quat.w(),
+        accelX, accelY, accelZ, // Corrected lines
+        magX, magY, magZ,       // Corrected lines
+        gyroX, gyroY, gyroZ,
+        orientationY, orientationY, orientationZ,
+        temperatureC,
+        pressureKPA);
+
+  Serial.print("Sending: ");
+  Serial.println(radiopacket);
+
+  rf95.send((uint8_t *)radiopacket, strlen(radiopacket));
+  rf95.waitPacketSent();
+
+  Serial.println("Waiting for reply...");
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  if (rf95.waitAvailableTimeout(1)) {
+    if (rf95.recv(buf, &len)) {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+    } else {
+      Serial.println("Receive failed");
+    }
+  } else {
+    Serial.println("No reply, is there a listener around?");
+  }
+  
 
   int8_t boardTemp = bno.getTemp();
   Serial.println();
@@ -116,62 +148,4 @@ void readSensor1Data() {
 
   Serial.println("--");
   delay(BNO055_SAMPLERATE_DELAY_MS);
-}
-
-void printEvent(sensors_event_t* event) {
-  float x = -1000000, y = -1000000, z = -1000000;
-
-  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
-    sendRadioTransmission("Accl:", event->acceleration.x, event->acceleration.y, event->acceleration.z);
-  } else if (event->type == SENSOR_TYPE_ORIENTATION) {
-    sendRadioTransmission("Orient:", event->orientation.x, event->orientation.y, event->orientation.z);
-  } else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
-    sendRadioTransmission("Mag:", event->magnetic.x, event->magnetic.y, event->magnetic.z);
-  } else if (event->type == SENSOR_TYPE_GYROSCOPE) {
-    sendRadioTransmission("Gyro:", event->gyro.x, event->gyro.y, event->gyro.z);
-  } else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
-    sendRadioTransmission("Rot:", event->gyro.x, event->gyro.y, event->gyro.z);
-  } else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
-    sendRadioTransmission("Linear:", event->acceleration.x, event->acceleration.y, event->acceleration.z);
-  } else if (event->type == SENSOR_TYPE_GRAVITY) {
-    Serial.print("Gravity:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  } else {
-    sendRadioTransmission("Unk:", x, y, z);
-  }
-}
-
-void sendRadioTransmission(const char* sensorType, float x, float y, float z) {
-  Serial.print(sensorType);
-  Serial.print("\tx= "); Serial.print(x);
-  Serial.print(" |\ty= "); Serial.print(y);
-  Serial.print("\t|\tz= "); Serial.println(z);
-
-  char radiopacket[30];
-  sprintf(radiopacket, "%s\tx=%.2f |\ty=%.2f |\tz=%.2f", sensorType, x, y, z);
-  
-  Serial.print("Sending: ");
-  Serial.println(radiopacket);
-
-  rf95.send((uint8_t *)radiopacket, strlen(radiopacket));
-  rf95.waitPacketSent();
-
-  Serial.println("Waiting for reply...");
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
-
-  if (rf95.waitAvailableTimeout(1000)) {
-    if (rf95.recv(buf, &len)) {
-      Serial.print("Got reply: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
-    } else {
-      Serial.println("Receive failed");
-    }
-  } else {
-    Serial.println("No reply, is there a listener around?");
-  }
 }
